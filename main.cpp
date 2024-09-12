92a6c074e32089fb4ff8322944346eb36153fe9e
MatheusContiero <matheusbcon@gmail.com>
2024-09-12 00:51:06 +0000

    adição

--- main.cpp
+++ main.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
+#include "Pessoa.cpp"
+using namespace std; 
+
+// g++ main.cpp -> rodar só um arquivo declarado pelo nome colocado
+// g++ *.cpp -> rodar mais de um arquivo
+// ./a.out
 
 int main (){
-    std::cout << "Hello world\n";
+    Pessoa p;
+    p.setNome("Nome");
+    p.setSobrenome("sobrenome");
+    p.setCPF(123456789);
+
+    cout << "Dados da pessoa" << endl;
+    cout << "Nome: " << p.getNome() << " " << p.getSobrenome() << endl;
+    cout << "CPF: " << p.getCPF() << endl;
 }
